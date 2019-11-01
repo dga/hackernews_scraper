4466150409954699911e76fcffb6bdeb22c53352
dga <dga@danielgarnold.com>
2019-10-31 21:05:49 -0700

    Remove arg.py

--- arg.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-e", "--echo", help="echo stuff", action="store_true")
-args = parser.parse_args()
-
-if args.echo:
-    print(args.echo)
